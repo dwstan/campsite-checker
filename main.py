434c9a37536ec75ca0bd6dac0a71606b3712dc01
I_Derp <dstan4321@gmail.com>
2024-07-20 08:00:56 -0700

    okay

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 timeout = 1 # how many minutes between checks
 dates = ["2024-07-26","2024-07-27", "2024-07-28"] # the start date you want to check availability for
-campground_ids = {"232447"} # the campground ids you want to check availability for
+campground_ids = {"232450", "232449", "232447"} # the campground ids you want to check availability for
 
 avail_checker(timeout, dates, campground_ids)
